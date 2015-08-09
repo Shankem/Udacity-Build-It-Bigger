a70491bc45bf81fac7e74bf615e7ecd33a7e6fa3
Pierce <pierce@growingcoder.com>
2015-08-09 10:08:27 -0600

    Added test for joke retriever.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,13 +1,36 @@
 package com.udacity.gradle.builditbigger;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.test.AndroidTestCase;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Tests the app to make sure we can retrieve a joke from the server.
+ * NOTE: Make sure the server is running before running the test.
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest extends AndroidTestCase {
+
+    private final CountDownLatch mSignal = new CountDownLatch(1);
+
+    public void testJokeRetriever() {
+        new JokeRetrieverTask(new TestJokeListener()).execute();
+        try {
+            boolean success = mSignal.await(5, TimeUnit.SECONDS);
+            if (!success) {
+                fail("Test timed out, make sure the server is actually running.");
+            }
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
+    private class TestJokeListener implements JokeRetrieverTask.OnJokeRetrievedListener {
+
+        @Override
+        public void onJokeRetrieved(String joke) {
+            assertTrue(joke != null && joke.length() > 0);
+            mSignal.countDown();
+        }
     }
 }
